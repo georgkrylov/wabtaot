cffe4ce6faf35b2bc69ad92dabe10acdd3d7bf44
Georgiy Krylov <georgiy.krylov@unb.ca>
2018-09-17 16:57:13 -0300

    four main test files with corresponding changes in wasm source code - commenting the export lines out.

---
+++ i64_bitwise_main.cc
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+
+extern "C" int64_t func_0();
+extern "C" int64_t func_1();
+extern "C" int64_t func_2();
+extern "C" int64_t func_3();
+extern "C" int64_t func_4();
+extern "C" int64_t func_5();
+extern "C" int64_t func_6();
+extern "C" int64_t func_7();
+extern "C" int64_t func_8();
+extern "C" int64_t func_9();
+extern "C" int64_t func_10();
+extern "C" int64_t func_11();
+extern "C" int64_t func_12();
+extern "C" int64_t func_13();
+extern "C" int64_t func_14();
+extern "C" int64_t func_15();
+extern "C" int64_t func_16();
+extern "C" int64_t func_17();
+extern "C" int64_t func_18();
+extern "C" int64_t func_19();
+extern "C" int64_t func_20();
+extern "C" int64_t func_21();
+extern "C" int64_t func_22();
+extern "C" int64_t func_23();
+extern "C" int64_t func_24();
+
+int main() {
+	//remember it is one off!
+	assert(func_0() == 17 );
+	assert(func_1() == 0);
+	assert(func_2() == 23 );
+	assert(func_3() == 18446744073709551615U );
+	assert(func_4() == 6);
+	assert(func_5() == 18446744073709551615U );
+	assert(func_6() == 1);
+	assert(func_7() == 8);	
+	assert(func_8() == 2);
+	assert(func_9() == 16 );
+	assert(func_10() == 2);
+	assert(func_11() == 8);
+	assert(func_12() == 18446744073709551615U);
+	assert(func_13() == 16 );
+	assert(func_14() == 2);
+	assert(func_15() == 8);	
+	assert(func_16() == 18014398509481983);
+	assert(func_17() == 1);
+	assert(func_18() == 32 );
+	assert(func_19() == 562958543486978);
+	assert(func_20() == 2305878194122661888 );
+	assert(func_21() == 16 );
+	assert(func_22() == 2);
+	assert(func_23() == 576469548530665472 );
+	assert(func_24() == 9223512776490647552U );
+
+}
